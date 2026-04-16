300a08876895ac783fc5f8496113b57c084cabe0
Isaac Letcher <iletcher@samford.edu>
2026-04-16 16:12:07 +0000

    updated settings.py and requirements.txt to with necessary python packages and establish connection to postgre database. Also created Django app and generated model classes based on postgre database.

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,12 +84,19 @@ WSGI_APPLICATION = "hello_world.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+database_url = config("DATABASE_URL", default="")
+
+if database_url:
+    DATABASES = {
+        "default": dj_database_url.config(default=database_url, conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
